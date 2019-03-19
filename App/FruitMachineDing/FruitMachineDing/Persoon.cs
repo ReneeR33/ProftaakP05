2ce97c30fbf14fb2efd6555404d437066f42c039
SleepingKraken <sleepingkraken@hotmail.com>
2019-03-19 15:28:24 +0100

    Hit Me Baby One More Time

--- App/FruitMachineDing/FruitMachineDing/Persoon.cs
+++ App/FruitMachineDing/FruitMachineDing/Persoon.cs
@@ -8,24 +8,9 @@ namespace FruitMachineDing
 {
     class Persoon
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         public List<string> porties = new List<string>();
         public Portie currentPortie { get; private set; } = new Portie();
 
->>>>>>> 97bd11f47a704738a32ba73ea1b35cc13635ec8c
-=======
-        public List<string> porties = new List<string>();
-        public Portie currentPortie { get; private set; } = new Portie();
-
->>>>>>> 73dc505dee249d0655ea1c1566273e8d30095dc5
-=======
-        public List<string> porties = new List<string>();
-        public Portie currentPortie { get; private set; } = new Portie();
-
->>>>>>> 73dc505dee249d0655ea1c1566273e8d30095dc5
         int vitaminesNodig;
         int vitaminesBinnen;
         int calorienBinnen;
@@ -48,48 +33,7 @@ namespace FruitMachineDing
         }
         public bool CheckVitamins()
         {
-<<<<<<< HEAD
-            //vitaminesBinnen=sql check vitamines
-            //vitaminesNodig=sql check vitamines die nodig zijn
-            if (vitaminesBinnen > vitaminesNodig)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-=======
-            //vitaminesNodig=sql check vitamines
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            //sql check vitamines die nodig zijn
-            if (vitaminesBinnen > vitaminesNodig)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-=======
-=======
->>>>>>> 73dc505dee249d0655ea1c1566273e8d30095dc5
-=======
->>>>>>> 73dc505dee249d0655ea1c1566273e8d30095dc5
-            ////sql check vitamines die nodig zijn
-            //if (vitaminesBinnen > vitaminesNodig)
-            //{
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
             return (vitaminesBinnen > vitaminesNodig);
-
->>>>>>> 97bd11f47a704738a32ba73ea1b35cc13635ec8c
         }
     }
 }
--- App/FruitMachineDing/FruitMachineDing/Portie.cs
+++ App/FruitMachineDing/FruitMachineDing/Portie.cs
@@ -12,23 +12,17 @@ namespace FruitMachineDing
         private int calories=0;
         public List<string> fruit = new List<string>();
         
-<<<<<<< HEAD
-        public static List<string> AddToList(string fruit)
-        {
-            portie.Add(fruit);
-            return portie;
-=======
         public void AddToList(string Fruit)
         {
             fruit.Add(Fruit);
->>>>>>> 97bd11f47a704738a32ba73ea1b35cc13635ec8c
         }
+
         public int CalculateCalories()
         {
             foreach(string Fruit in fruit)
             {
                 //sql get calories 
-//                calories += //sql geef calories van fruit met naam fruit
+                //calories += //sql geef calories van fruit met naam fruit
             }
             return calories;
         }
