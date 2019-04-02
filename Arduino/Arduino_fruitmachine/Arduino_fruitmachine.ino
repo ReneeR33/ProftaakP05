8d638476a11b31b8abbc39bc0d20bba0bf5adea0
Renée <42935816+ReneeR33@users.noreply.github.com>
2019-04-02 13:24:03 +0200

    vingerprint toevoegen

--- Arduino/Arduino_fruitmachine/Arduino_fruitmachine.ino
+++ Arduino/Arduino_fruitmachine/Arduino_fruitmachine.ino
@@ -1,11 +1,22 @@
+String Message = "ADD_FINGERPRINT";
+uint8_t ID = 2;
 void setup() {
   Serial.begin(9600);
   StartFingerprintscanner();
 }
 
 void loop() {
-  String Message = CheckMessage();
-  ReadFingerprint();
+  //String message = CheckMessage();
+  if (Message != "ADD_FINGERPRINT") {
+    ReadFingerprint();
+  }
+  else if (Message == "ADD_FINGERPRINT") {
+    Serial.print("Enrolling ID #");
+    Serial.println(ID);
+    AddFingerprint(ID);
+    Message = "";
+  }
+
   delay(50);
 
 }
