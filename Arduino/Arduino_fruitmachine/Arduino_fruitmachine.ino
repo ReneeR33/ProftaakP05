ee262117ac72473d5df89dc0c9d84c4d1518181b
Renée <42935816+ReneeR33@users.noreply.github.com>
2019-04-02 11:43:07 +0200

    vingerprint

--- Arduino/Arduino_fruitmachine/Arduino_fruitmachine.ino
+++ Arduino/Arduino_fruitmachine/Arduino_fruitmachine.ino
@@ -1,9 +1,11 @@
 void setup() {
-  
-
+  Serial.begin(9600);
+  StartFingerprintscanner();
 }
 
 void loop() {
-  
+  String Message = CheckMessage();
+  ReadFingerprint();
+  delay(50);
 
 }
--- Arduino/Arduino_fruitmachine/Vingerprintscanner.ino
+++ Arduino/Arduino_fruitmachine/Vingerprintscanner.ino
@@ -1,1 +1,30 @@
+#include <Adafruit_Fingerprint.h>
 
+SoftwareSerial mySerial(2, 3);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void StartFingerprintscanner() {
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  Serial.println("Waiting for valid finger...");
+}
+
+int ReadFingerprint() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("|Fingerprint_Detected:");
+  Serial.print(finger.fingerID);
+  Serial.println("%");
+  return finger.fingerID; 
+}
