6924c905b22dba9aadb273bc4c160c6c4fd373f2
Renée <42935816+ReneeR33@users.noreply.github.com>
2019-04-02 14:24:42 +0200

    motor?

--- Arduino/Arduino_fruitmachine/EV3Shield.ino
+++ Arduino/Arduino_fruitmachine/EV3Shield.ino
@@ -2,21 +2,31 @@
 #include <EVShield.h>
 
 EVShield          evshield(0x34, 0x36);
+int currentDisk = 1;
 
-void RunMotorA(int Degrees, int MotorSpeed){
+void RunMotor1(int Degrees, int MotorSpeed, int Direction){
    evshield.bank_b.motorRunDegrees(SH_Motor_1,
-                                    SH_Direction_Forward,
+                                    Direction,
                                     MotorSpeed,
                                     Degrees,
                                     SH_Completion_Wait_For,
                                     SH_Next_Action_BrakeHold);
 }
 
-void RunMotorB(int Rotations, int MotorSpeed){
+void RunMotor2(int Rotations, int MotorSpeed){
   evshield.bank_b.motorRunRotations(SH_Motor_2,
                                     SH_Direction_Forward,
                                     MotorSpeed,
                                     Rotations,
                                     SH_Completion_Wait_For,
                                     SH_Next_Action_BrakeHold);
+}
+
+void SwitchToDisk(int disk){
+  if((disk = currentDisk + 1 && disk != 3) || (disk == 1 && currentDisk == 3)){
+    RunMotor1(120, 8, SH_Direction_Forward);
+  }
+  else{
+    RunMotor1(120, 8, SH_Direction_Reverse);
+  }
 }
