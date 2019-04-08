1399c57188843a47e6ce0a63ab976c8598f9b8ec
SleepingKraken <sleepingkraken@hotmail.com>
2019-04-08 11:01:36 +0200

    Waarom is git zo sheit

--- App/FruitMachineDing/FruitMachineDing/Fruit.cs
+++ App/FruitMachineDing/FruitMachineDing/Fruit.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace FruitMachineDing
 {
     class Fruit
     {
+        public string GiveDescription(string connectionString, string naam)
+        {
+            string query = "SELECT descriptie FROM Fruit WHERE Name = '"+naam+"'";
 
+            using (SqlConnection conn = new SqlConnection(Convert.ToString(connectionString)))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return reader.GetString(0);
+                    }
+                    else
+                    {
+                        return "unknown";
+                    }
+                }
+            }
+        }
     }
 }
