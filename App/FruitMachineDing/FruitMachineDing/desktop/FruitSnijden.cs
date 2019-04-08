4d4f024cb5b2ca908eabb2031e3710da289f18c7
SleepingKraken <sleepingkraken@hotmail.com>
2019-04-08 16:57:23 +0200

    yee

--- App/FruitMachineDing/FruitMachineDing/desktop/FruitSnijden.cs
+++ App/FruitMachineDing/FruitMachineDing/desktop/FruitSnijden.cs
@@ -27,7 +27,7 @@ namespace FruitMachineDing
             InitializeComponent();
             connectionString = ConfigurationManager.ConnectionStrings["FruitMachineDing.Properties.Settings.FruitDBConnectionString"].ConnectionString;
             this.FormClosed += new FormClosedEventHandler(FormFruitSnijden_FormClosed);
-            serial.Connect();
+            //serial.Connect();
             
         }
 
